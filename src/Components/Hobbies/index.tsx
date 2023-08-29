c3218a8d1e94c10dc7d21d50a4f0d169a60d1c58
Kunal Bansal <kunal@immertec.com>
2023-08-29 15:23:26 -0400

    init modules

---
+++ src/Components/Hobbies/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import logo from "../../logo.svg";
+
+function Hobbies() {
+    return <header className="App-header">
+    <img src={logo} className="App-logo" alt="logo" />
+    <p>
+      Edit <code>src/App.tsx</code> and save to reload.
+    </p>
+  </header>;
+}
+
+export default Hobbies;
